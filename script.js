8a87c5eedf9cee0c853aa242c5ca5ee539829d96
Shifa <luh.shifa@gmail.com>
2025-01-16 18:17:49 +0800

    Thu, Jan 16 2025

---
+++ script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// deklarasikan variable
+// addEventListerner ketika button di click
+// tampilkan form input
+
+const form = document.querySelector(".form");
+const table = document.querySelector(".table-container");
+const btnCreate = document.querySelector(".form-btn-create");
+const btnEdit = document.querySelector(".form-btn-edit");
+const btnDelete = document.querySelector(".form-btn-delete");
+
+// event
+btnCreate.addEventListener("click", function () {});
