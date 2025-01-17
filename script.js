b3b9541be6420af16f904095a69dca921051a852
Shifa <luh.shifa@gmail.com>
2025-01-17 18:24:47 +0800

    Friday, Jan 17 2025

--- script.js
+++ script.js
@@ -9,6 +9,53 @@ const table = document.querySelector(".table-container");
 const btnCreate = document.querySelector(".form-btn-create");
 const btnEdit = document.querySelector(".form-btn-edit");
 const btnDelete = document.querySelector(".form-btn-delete");
+const containerTableRow = document.querySelector(".table-row");
+
+const conc1 = {
+  name: "Jamal",
+  phone: 9878129,
+  sex: "Male",
+  address: "Rumah Mevvah",
+};
+
+const conc2 = {
+  name: "Komi",
+  phone: 9001292,
+  sex: "Female",
+  address: "Rumah Peri",
+};
 
 // event
-btnCreate.addEventListener("click", function () {});
+
+class PersonContact {
+  // array untuk menampung semua data
+  #contacts = [];
+  // untuk param address, isi dulu dengan default value, karena ini param optional
+  constructor(name, phone, sex, address = "rumah") {
+    this.name = name;
+    this.phone = phone;
+    this.sex = sex;
+    this.address = address;
+  }
+
+  // tampilkan data yang ada di local storage
+  view() {
+    containerTableRow.innerHTML = "";
+    const html = `
+    <td>${this.name}</td>
+    <td>09001292</td>
+    <td>Female</td>
+    <td>Jln A.Yani Barat</td>
+    `;
+  }
+
+  create() {
+    btnCreate.addEventListener("click", function (e) {
+      e.preventDefault();
+      form.classList.remove("hidden");
+    });
+  }
+}
+
+const contact = new PersonContact();
+contact.create();
